2a24a096f47b8a87effd6679fea1dd8761fb9dc7
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-26 23:34:55 +0300

    rating 800

---
+++ TheWordsAreTooLong71A.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TheWordsAreTooLong71A {
+    public static void main(String[] args) {
+        final int SHORT_LEN = 10;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] array = new String[n];
+        for (int i = 0; i < n; i++) {
+            String word = sc.nextLine();
+            array[i] = word;
+        }
+        sc.close();
+
+        for (String word : array) {
+            StringBuilder newWord = new StringBuilder();
+            if (word.length() <= SHORT_LEN) {
+                System.out.println(word);
+            } else {
+                char[] charWord = word.toCharArray();
+
+                for (int j = 0; j < charWord.length; j++) {
+                    if (j == charWord.length - 2) {
+                        newWord.append(charWord.length - 2);
+                    }
+                    if (j == 0) {
+                        newWord.append(charWord[j]);
+                    }
+                    if (j == charWord.length - 1) {
+                        newWord.append(charWord[j]);
+                    }
+                }
+                System.out.println(newWord);
+            }
+
+        }
+    }
+}
