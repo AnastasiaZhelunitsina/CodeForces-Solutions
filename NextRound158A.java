955742c457ebbb901cfe1d90cc91fcf4fc70fe7d
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-27 23:30:16 +0300

    rating 800

---
+++ NextRound158A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NextRound158A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] buff = sc.nextLine().split(" ");
+        int n = Integer.parseInt(buff[0]);
+        int k = Integer.parseInt(buff[1]);
+        String[] tempScore = sc.nextLine().split(" ");
+        sc.close();
+
+        int counter = 0;
+
+        for (int i = 0; i < tempScore.length; i++) {
+            String score = tempScore[i];
+            if (Integer.parseInt(score) > 0 && Integer.parseInt(score) >= Integer.parseInt(tempScore[k - 1])) {
+                counter++;
+            }
+
+        }
+        System.out.println(counter);
+    }
+}
