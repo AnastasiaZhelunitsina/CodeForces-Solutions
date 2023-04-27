f909286ded37731bde2e37f6cd29bc24ad8c3cd1
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-27 22:32:42 +0300

    rating 800

---
+++ Team231A.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Team231A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            String[] temp = sc.nextLine().split(" ");
+            int elemSum = 0;
+            for (String elem : temp) {
+                elemSum += Integer.parseInt(elem);
+            }
+            array[i] = elemSum;
+        }
+        sc.close();
+
+        int counter = 0;
+        for (int num : array) {
+            if (num >= 2) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+}
