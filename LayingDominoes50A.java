badc2edf0d54fd1762cf1110fcbabb63f32920cb
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-27 23:44:23 +0300

    rating 800

---
+++ LayingDominoes50A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LayingDominoes50A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] temp = sc.nextLine().split(" ");
+        sc.close();
+
+        int m = Integer.parseInt(temp[0]);
+        int n = Integer.parseInt(temp[1]);
+
+        int rectangleArea = m * n;
+
+        int dominoes = rectangleArea / 2;
+        System.out.println(dominoes);
+    }
+}
