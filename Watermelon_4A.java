6b9b0c5219397d51c9728a39eb4c370394d0961f
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-29 00:33:04 +0300

    rating 800

--- Watermelon_4A.java
+++ Watermelon_4A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Watermelon4A {
+public class Watermelon_4A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
