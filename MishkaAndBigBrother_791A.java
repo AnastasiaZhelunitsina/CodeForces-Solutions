a5bf5884b41959df7b5d4916b9b7798fc772a851
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-30 18:16:19 +0300

    rating 800
    791A - Bear Cub and big brother
    791A - Медвежонок и старший брат

---
+++ MishkaAndBigBrother_791A.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MishkaAndBigBrother_791A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String buff = sc.nextLine();
+        sc.close();
+        String[] splitBuff = buff.split(" ");
+
+        int weightLimak = Integer.parseInt(splitBuff[0]);
+        int weightBob = Integer.parseInt(splitBuff[1]);
+
+        int result = 0;
+
+        while (true) {
+            if (weightLimak <= weightBob) {
+                weightLimak *= 3;
+                weightBob *= 2;
+                result++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
