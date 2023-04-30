8e7491a63eef62148b7d40293c9b098e1372ac59
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-30 20:00:20 +0300

    rating 800
    977A - Incorrect subtraction
    977A - Неправильное вычитание

---
+++ IncorrectSubtraction_977A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IncorrectSubtraction_977A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] line = sc.nextLine().split(" ");
+        sc.close();
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+
+        for (int i = 0; i < k; i++) {
+            int temp = n % 10;
+            if (temp != 0) {
+                n -= 1;
+            } else {
+                n /= 10;
+            }
+        }
+        System.out.println(n);
+    }
+}
