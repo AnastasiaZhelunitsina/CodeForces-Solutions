bb50a8463102be73ac621f7259114a6bd1dd696c
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-04-30 16:38:32 +0300

    rating 800
    281A - Capitalization of the word
    281A - Капитализация слова

---
+++ CapitalizationOfTheWord_281A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CapitalizationOfTheWord_281A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        sc.close();
+        StringBuilder newLine = new StringBuilder();
+        char[] charLine = line.toCharArray();
+        for (int i = 0; i < charLine.length; i++) {
+            String elem = String.valueOf(charLine[i]);
+            if (i == 0) {
+                newLine.append(elem.toUpperCase());
+            } else {
+                newLine.append(elem);
+            }
+        }
+        System.out.println(newLine.toString());
+    }
+}
