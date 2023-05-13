0930250d5e4e9ddb49549d35320e0b53cdb5c490
AnastasiaZhelunitsina <78534796+AnastasiaZhelunitsina@users.noreply.github.com>
2023-05-13 17:49:56 +0300

    rating 900
    96A - Football
    96A - Футбол

---
+++ Football_96A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Football_96A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        if (str.contains("1111111") || str.contains("0000000")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
